9b19121d46baf458cb9ec20cdb8bb03141a6a0f0
Razafindriatsara <mrtnraza@gmail.com>
2026-07-07 13:34:01 +0200

    Mise à jour des bibliothèques pour Flutter 3.44

--- lib/services/audio_uri_web.dart
+++ lib/services/audio_uri_web.dart
@@ -1,9 +1,10 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'dart:js_interop';
 import 'dart:typed_data';
+
+import 'package:web/web.dart' as web;
 
 /// Web : crée une URL blob à partir des octets.
 Future<Uri> prepareAudioUri(String name, Uint8List bytes) async {
-  final blob = html.Blob([bytes]);
-  return Uri.parse(html.Url.createObjectUrlFromBlob(blob));
+  final blob = web.Blob([bytes.toJS].toJS);
+  return Uri.parse(web.URL.createObjectURL(blob));
 }
